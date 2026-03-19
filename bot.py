9f80bdd426b03a0a531bc76668f9dc6fef4cfa6f
eborodina235874-sketch <eborodina235874@gmail.com>
2026-03-19 22:17:47 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 import os
 import pandas as pd
 from datetime import datetime
-from maxapi import Bot, Dispatcher, types
-from maxapi.fsm import State, StatesGroup, FSMContext
+from pyromax import Bot, Dispatcher, types
+from pyromax.fsm import State, StatesGroup, FSMContext
 
 TOKEN = "f9LHodD0cOLppXCKzWiAHSfTq-lgD881ak2ktA96D8sGuWkLjwQpwbxsIXfN5vIT77T04dOmohcoqynHQSUR"
 EXCEL_FILE = "pokazaniya.xlsx"
